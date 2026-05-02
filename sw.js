066ead300b3e8d0ff8c55559811aed9dd6be3443
nayaraitapiuna-commits <nayaraitapiuna@gmail.com>
2026-05-02 12:41:58 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,35 +1,8 @@
-const CACHE = 'acaicontrol-v1';
-const ASSETS = [
-  '/',
-  '/index.html',
-  'https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700;800&display=swap'
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
-  );
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => {
-      if (cached) return cached;
-      return fetch(e.request).then(res => {
-        if (res && res.status === 200) {
-          const clone = res.clone();
-          caches.open(CACHE).then(c => c.put(e.request, clone));
-        }
-        return res;
-      }).catch(() => caches.match('/index.html'));
-    })
-  );
+// Desregistra qualquer service worker anterior
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', () => {
+  self.clients.matchAll({type:'window'}).then(clients => {
+    clients.forEach(c => c.navigate(c.url));
+  });
+  return self.clients.claim();
 });
